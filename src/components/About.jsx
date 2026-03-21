530213909f77de31dd6b98ae0f51bf7253ee35ba
Jaison A <jaison2005k@gmail.com>
2026-03-21 14:55:29 +0530

    Fixed an error in modifing the file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import profileImg from './assets/profile.jpg';
+import profileImg from '../assets/profile.jpg';
 function About() {
   return (
     <section id="about" className="about">
